e50ee705f63fc034e17723872c83972b0794dcdf
devingilmorecp@gmail.com <devingilmorecp@gmail.com>
2016-12-13 07:32:32 -0500

    Standard functions and a running list of useful tools.  Should probably consult that about the data frame problem.

---
+++ Standard9.R
@@ -0,0 +1,398 @@
+#Attempting a standard set of data analysis tools for use with R
+#Phaser:  AUTOMATIC DATA SELECTOR - FIRE AND FORGET
+#STANDARD SCRIPT
+
+Phaser<- function(fp) {
+  psav<- grep("\\.sav",fp)
+  pcsv<- grep("\\.csv",fp)
+  pdbf<- grep("\\.dbf",fp)
+  dval<- if (length(psav)>0) {library(memisc)
+    data.frame(as.data.set(spss.system.file(fp)))}
+    if (length(psav)>0) return(dval)
+  cval<-   if (length(pcsv)>0) (read.csv(fp,sep=",",header=T,as.is=T))
+    if (length(pcsv)>0) return(cval)
+  dbfl<- if (length(pdbf>0)) {library(foreign)
+    read.dbf(fp,as.is=T)}
+    if (length(pdbf)>0) return(dbfl)
+}
+#TO-DO LIST COMPLETED.  
+#Currently accepts csv sav and dbf files
+#DBF Automatically inputs with header
+#CSV Assumes header and you want strings and not factors
+
+#Takes a vector vnum defining the df columns to be dropped
+#and creates a frame  dropping it
+Judo<- function(df,vnum) {
+gbomb<- names(df)
+dv<- gbomb[vnum]
+    rv<- df[ , !(names(df) %in% dv)]
+rv
+} 
+
+#########DATA PRESENTATION:  XTABLE
+mstab<- data.frame(Variable=ttnam,NoHE_MSD=szmpair,HE_MSD=sospair,Statistic=ttval,p=ttps)
+#xtable(mstab,caption="Means, SDs, and independent samples T-tests #by whether the patient saw a health educator or not.")
+ncn<- c("M(SD): No HE","M(SD): Saw HE")
+#Trying to set sanitized new column names
+colnames(mstab)[2:3]<- ncn
+#Success!  This method allows for setting nonstandard column names in an Xtable.
+xtable(mstab,caption="Means, SDs, and independent samples T-tests by whether the patient saw a health educator or not.")
+
+
+
+
+####DATA MANIPULATION TOOLS@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+###############################@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+rm(list=ls())
+###Datamanagement 
+qt<- table(pr$Q..)
+#Basic frequencies for the variable
+ltobj$studname<- append(ltobj$studname,ln)
+#Append command adds second argument to the end of the first
+#Must be a vector
+assign(fivecode[1],28000)
+
+ntmane<- which(nums == "TRUE")
+
+#Need a missing data handler
+nomiss<- na.omit(fud)
+
+#This works on data frames - data frame is apparently a type of list
+mfudi<- sapply(fudi,mean)
+
+
+#TO-DO:  
+  #1(  LARGE DATASETS - Add instructions for ffdf
+
+
+  #2(  #STANDARD PROGRAM
+    #Point it at a data file, and it determines whether the file is dat, csv, spss, or some other format
+    #It defaults to the preferred method, memisc for spss, read,table for a dat, and what im asssuming is read.csv
+    #WITH STRINGSTOFACTORS OFF
+
+#VECTOR
+fv<- dv[-8]
+  #Fv dv minus vector 8
+
+#Identify duplicates in crs
+dcrs<- duplicated(crs)
+#This will return a boolean you turn it to numeric
+ncrs<- as.numeric(dcrs)
+nrnc<- which(ncrs==1)
+#Then use Which to identify the vector numbers which are duplicated
+
+
+###SCRIPTS
+
+#Write a dat for mplus
+write.table(c1k,"C:/shanteria/imputation/main.dat",sep="\t",
+  row.names=FALSE,
+  col.names=FALSE)
+
+nfile<- names(c1k)
+nfile
+write.table(nfile,"C:/shanteria/imputation/varlist.csv",sep=",",
+  row.names=FALSE,
+  col.names=FALSE)
+c1k$Perp_gender_1
+c1k$Perp_gender_2
+
+#BENCHMARKING
+system.time(expr, gcFirst = TRUE) #For benching an expr
+Sys.time()#For direct access to the system time
+
+###################STRINGS
+
+nbc<- gsub("\\.","",ncb)
+#String substitution:  Substitutes period with nothing in vector ncb
+
+#####PROVISIONAL SCRIPT
+#Using the textminer package for extracting PDF Info
+#IN PROGGRESS
+#ARTICLERIPPER:
+#1)   Add information about an article easily.  
+#2)  Pull info from article?
+#Background information:
+library(tm)
+dirname<- file.path("C:","rhandler")
+doccorp<- Corpus(DirSource(dirname))
+summary(doccorp)
+#Creating document term matrix see https://rstudio-pubs-static.s3.amazonaws.com/31867_8236987cf0a8444e962ccd2aec46d9c3.html
+dtmcorp<- DocumentTermMatrix(doccorp)
+tdmcorp<- TermDocumentMatrix(doccorp)
+library(wordcloud)
+#Word frequencies?
+freq<- colSums(as.matrix(dtmcorp))
+str(dtmcorp)
+dtmcorp$dimnames
+dtmcorp$dimnames$Docs
+inspect(dtmcorp$dimnames$Docs[8])
+pdf1<- readPDF("C:/rhandler/pfex.pdf")
+#What a fucking PITA package....
+
+
+
+#END SCRIPTS
+#Merging################## ->>>>>>>>>>>>>>><___________________
+
+
+
+
+
+#####################factors
+
+library(plyr)
+#Renaming factor levels http://www.cookbook-r.com/Manipulating_data/Renaming_levels_of_a_factor/
+revalue(lab, c("Health Problems and Self-Medicating" = "Health and Self-Medicating"))
+#WHOA THAT WAS EASY!
+
+######################LISTSL
+#Working with lists.
+#RULE #1:  know to unlist!   It's awesome!
+unlist(listname) #This breaks stuff down into a vector.  
+#List creation. 
+Basiclist<- list(Name=c("Carol","Linda","Stacy"), Value=c(1000,2812,1233))
+#Index list of names
+Basiclist$Name[]
+#Index Name #2
+Basiclist$Name[[2]]
+#FUNCTION WRAP:  Add a value to basiclist
+Addvalue<- function(Listname,wakevalue) {
+  ln<- length(Listname$Value)
+  Listname$Value[[ln+1]]<- wakevalue
+  Listname
+}
+#Attempt1
+Adding10<- Addvalue(Basiclist,10)
+Adding10$Value[[4]]
+#COMPLETD.  
+#Cycling 
+Adding192<- Addvalue(Adding10,192)
+Adding192
+Addingsane<- Addvalue(Adding192,Adding192$Name[[2]])
+Addingsane
+#Addingsane added Linda to the end of value list AND transformed
+#The VALUE VARIABLE to a chr vector.
+
+Adding20<- Adding10$Value[[3]]^Adding10$Value[[1]]
+Adding20
+#Returns inf, sweet!
+#########################END LISTS
+
+#COMMANDS
+ls() #List objects in the environment
+obj<- ls()
+obj[3]
+
+Basiclist
+Basiclist$sortval<- sort(Basiclist$Value)
+Basiclist$valindi<- order(Basiclist$Value)
+#Sort the frame on value
+
+
+#-----------------------------stats functions-----------------------------
+#Clear workspace.  
+rm(list=ls())
+rnormal<- rnorm(100) #Random values on the standard normal 
+hist(rnormal)
+
+#Simple correlation
+core<- cor(df1,df2)
+
+
+####TO-DO - STANDARD REQUIREMENTS
+#STANDARD script should be able to rip out each variable in a dataset, and graph
+#Against every other variable and spit the resulting plots into a folder 
+
+
+####SCAN FUNCTIONality
+#PLAYING.
+
+#SCAN:  TEXT FILE
+scantext<- scan("C:/rhandler/test1.txt", what="")
+scantext
+#scantext is now a vector of strings , one for each word, or contiguous string 
+
+#We can also scan in one line at a time for each vector
+scanlines<- scan("C:/rhandler/test1.txt", what="", sep="\n")
+scanlines
+
+#attempting scan of a PDF
+scanpdf<- scan("C:/rhandler/pfex.pdf",what="")
+#FAILSON
+#2-9-2016
+#SCANNING A NOTES FILE FOR PROCESSING ADVANTAGE
+yild<- scan("C:/rhandler/yild.txt",what="")
+yildline<- scan("C:/rhandler/yild.txt",what="",sep="\n")
+#COUNT the number of elements across clusters
+#EX. Vitality survey data.
+v14<- read.table("C:/rhandler/v142.csv", sep=",")
+v14
+str(v14)
+#V1 is a factor with 159 levels.  Obs are clustered within V1.
+#Isolating for count.
+
+#Problem - whitespace trails remain
+q15t$cty<- trimws(q15t$county, which="both")
+
+
+#clean - up this section
+cnames<- v14$V1
+str(cnames)
+cnames[114]
+test1<- table(cnames)
+test1
+str(test1)
+test1[14]
+dftest1<- data.frame(test1)
+dftest1
+write.table(dftest1, "C:/rhandler/vitalitycount.csv", sep=",")
+#Dftest 1 is a nice frequency table of county vitality responses.
+test2<- table(v14$V1, v14$V39)
+test2
+v142<- read.table("C:/rhandler/v14_2.csv")
+test3<- table(v142$V1, v142$V2)
+v142
+tes4<- ave(v14$V1, v14$V39)
+tes4
+bar2<- barplot(dftest1$Freq,col="#1437ac", ylab="How many members")
+d14<- data.frame(v14)
+d14
+d14[1]
+#CLOSE CLEANUP SECTION
+#LOOPS
+
+#Using "scanlines" variable above
+#Attempting to loop across the scanlines file and isolate all vectors containing a)
+fattempt<- function(x) {
+  aloc<- grep("a)", x)
+  extract<- x[]
+}
+
+#Element testing
+aloc<- grep("a)", scanlines)
+avec<- scanlines[aloc] #This performs the job very cleanly.  
+
+#GREP:  Exact matching
+gblocation421<- grep(searchgb[421], nzane,fixed=T)
+#STANDARD.
+
+extraction<- list()
+extraction$avalues<- rep(NA, length(x)) #empty list item 
+
+element1<- rep(0, length(scanlines))
+
+#End Loops
+
+#-------------------------TO DO:  Recode variables functions-------------#
+
+
+#-----------------------------END stats functions-----------------------------
+#------------------------------PLOTTING-----------------------------------
+#histogram - basic r (no packs )++++++++++++
+#++++++++++++++++++++++++++++++++++++++++++
+hdata<- c(3,5,7,1,3)
+hist1<- hist(hdata, col="#8bb8ea",ylab="Number of students")
+hdata2<- seq(from=75, to=100, by=2.2)
+hdata2
+hist2<- hist(hdata2, col="#8bb8ea", ylab="Number of students", xlab="Test Score")
+#Histogram, baby blue coloration.
+
+
+#BAR PLOT - BASIC R (no packages)+++++++++++++
+#+++++++++++++++++++++++++++++++++++++++++++++
+#These are the frequencies for the bar chart.
+colheight<- c(12,9,4,3,0)
+#Barplot with names, COLORED BARS
+bar2<- barplot(colheight, names.arg=c(5,4,3,2,1), col="#1437ac", 
+               xlab="Quiz Score", ylab="How many students")
+
+#Line graph of values across time
+#KEY IS THE TYPE="l" command
+xaxis<- seq(0, 100, length=100)
+line1<- plot(xaxis, (xaxis)^2, ylab="X-squared", xlab="Sequence", type="l")
+lines(xaxis, cos(xaxis), lty=3)
+
+#Scatter plot
+spam<- rnorm(200)
+sick<- rnorm(200)
+
+#Scatter plot with linear trend line
+ill<- plot(sick, spam, ylab="Spam Intake", xlab="Food-Borne Illnesses")
+abline(lm(sick ~ spam))
+
+ill<- plot(sick, spam, ylab="Spam Intake", xlab="Food-Borne Illnesses")
+abline(lm(sick ~ spam*spam))
+
+#$####################GGPLOT EXAMPLES------------------
+bas<-  ggplot(ac1, aes(x=ac1$year,y=ac1$amount,colour=ac1$fcode)) +
+    geom_point(size=3,shape=15) +
+    geom_line() + 
+    theme(axis.line.x = element_line(size=1,colour="black",linetype="solid")) + 
+    theme(axis.line.y = element_line(size=1,colour="black",linetype="solid")) + 
+    theme(panel.background=element_rect(fill='white',colour='white')) + 
+    labs(title=ac1$county[1],x="Year",y="Leveraged Cash ($)") +
+    scale_x_continuous(breaks=c(seq(2005,2015,by=1))) 
+#Geom line connects the pts here
+#Axis line options
+#Change bgcolor to white
+#Set breaks for the x axis
+
+#ggplot histograms
+
+#Data frames###########################################
+
+#Create an empty data frame.
+dtf1<- data.frame()
+#Add a variable
+dtf1$dollar<- c("$400", "$100", "$99,999", "$3.50")
+#This doesn't work, you have to rbind
+dtf1<- rbind(dtf1, c("$400", "$100", "$99,999", "$3.50"))
+str(dtf1) 
+#Each variable now called X..400, X..100, etc.
+
+#This badass script creates a cloned data frame that drops variable names listed in the vector dv
+c2<- clone[ , !(names(clone) %in% dv)]
+  
+
+########DATA MANAGEMENT#######################
+######RESHAPE
+library(reshape)
+#Attempting to melt test1fix by ID variable (FN)
+reshape1<- melt(test1fix, id=c("V1"), measure=c("V6", "V7"), 
+                variable_name="Originals")
+  #RESULT: Appears to have grouped V6 and V7 responses in long format
+str(reshape1) #Structure confirmed.  
+#LEARNING??????????????????????????????????????????????????????????
+
+#SEALED......STANDARD.#SEALED......STANDARD.#SEALED......STANDARD.#SEALED......STANDARD.
+#Now we need to compute the test score by summing V6 and V7 within ID
+#And dividing the products to derive the % correct.
+
+#Format for cast is ID ~ sum?
+castcheck<- cast(reshape1, Originals ~ value, sum)
+#Chick example 10-23-2015
+head(ChickWeight)
+chick_m<- melt(ChickWeight, id=2:4, na.rm=TRUE)
+head(chick_m)
+  #The id=2:4 identifies time chick and diet as id variables?
+  #The variable "weight" is now a row value in chick_m
+  #The value of weight is now given by the variable "value" in the row
+
+cast1<- cast(chick_m, Time ~ variable, mean)
+  #Cast 1 is now a simplified table containing all values of time, 
+  #And the mean weight within each time
+
+
+#R Markdown
+
+#INCLUDING FIGURES#############################
+
+<<c1fig,fig=T>>=
+yyy<- read.table("C:/Sustainability/GETFISCAL.csv",sep=",",stringsAsFactors=F)
+clist<- unique(yyy$county)
+c1<- yyy[yyy$county==clist[1],]
+ac1<- c1[c1$amount>0,]
+plot(ac1$year,ac1$amount)
+@
+  #############################################
---
+++ sfunctions.R
@@ -0,0 +1,317 @@
+############DNA TEST - SELF ASSESSMENT DATABASE BUILDER
+
+#DNALIST Takes a DIRECTORY, scans it for SPSS files, opens them up in a labelled list
+#NEXT STEP IS to execute a successful rbind after melt with these guys, which I think may require unfactoring county
+  #OR ~ WE Could still use the factors but just standardize them to be the same
+  #Not sure how that works thought.  
+DNALIST<- function(filepath) {
+  lfs<- list.files(filepath,recursive=T,full.names=T)
+  sns<- grep("\\.sav",lfs)
+  son<- lfs[sns]
+  sloan<- as.list(son)
+  pappy<- lapply(sloan,Phaser)
+  #Grab the file names Gsub the whitepsace 
+  #Then call pappy those names
+  sfs<- list.files(filepath)
+  sfns<- grep("\\.sav",sfs)
+  shortson<- sfs[sfns]
+  #These are the anmes
+  shortso<- gsub("\\.sav","",shortson)
+  shorts<- gsub(" ","_",shortso)
+  short<- gsub("-","_",shortso)
+  names(pappy) <- short
+  pappy
+}
+
+#EMHANCEMENT DNAX:  Automatic melting and rbind
+DNAX<- function(filepath) {
+  lfs<- list.files(filepath,recursive=T,full.names=T)
+  sns<- grep("\\.sav",lfs)
+  son<- lfs[sns]
+  sloan<- as.list(son)
+  pappy<- lapply(sloan,Phaser)
+  #Grab the file names Gsub the whitepsace 
+  #Then call pappy those names
+  sfs<- list.files(filepath)
+  sfns<- grep("\\.sav",sfs)
+  shortson<- sfs[sfns]
+  #These are the anmes
+  shortso<- gsub("\\.sav","",shortson)
+  shorts<- gsub(" ","_",shortso)
+  short<- gsub("-","_",shortso)
+  names(pappy) <- short
+mbold<- function(x) {melt(x, id="county")}
+detest<- lapply(pappy,mbold)
+detest
+}
+#######FAILED.
+
+############ATOMIZER - FILE REPORTING 
+ATOM<- function(dir) {
+  lfd<- list.files(dir, 
+    full.names=T,
+    recursive=T,
+    include.dirs=T)
+  
+  fin<- file.info(lfd)
+  fin$mb<- fin$size*1e-6
+  bigfin<- subset(fin,mb>=2000)
+  biglocator<- row.names(bigfin)
+  #Problem here: How do I recover the file names for the prinout?
+  
+  #Find different data file types
+  savl<- grep("\\.sav",lfd)
+  csvl<- grep("\\.csv",lfd)
+  datl<- grep("\\.dat",lfd)
+  dbfl<- grep("\\.dbf",lfd)
+  #Summary printout
+  savnum<- paste(length(savl),".sav files identified.\n")
+  csvnum<- paste(length(csvl),".csv files identified.\n")
+  datnum<- paste(length(datl),".dat files identified.\n")
+  dbfnum<- paste(length(dbfl),",dbf files identified.\n")
+  
+  printout<- c(savnum,csvnum,datnum,dbfnum)
+  #Printout is an inchoate summary of the contents. 
+  #One thing I would like to include is to return an object like
+  #data$csv which includes a vector of those files 
+  
+  sealone<- list(CSV=lfd[csvl],
+    SAV=lfd[savl],
+    DAT=lfd[datl],
+    DBF=lfd[dbfl],
+    BIGDATA=bigfin,
+    FULL_LIST=fin)
+  #It'd be nice to display file size alongside it for this obj
+sealone
+#NOTE:  Add text file detector.
+}
+
+
+
+
+########################################
+
+#Works
+#A wrapper for write.table to csv would be cool, one that dumps into a default directory 
+#With either tha object as fn or random name.
+
+#ATOMIZER
+  #SMASHES DATA Into elementary pieces
+  #Descriptives, missing data, etc.
+
+
+#1.0
+Judo<- function(df,vnum) {
+gbomb<- names(df)
+dv<- gbomb[vnum]
+    rv<- df[ , !(names(df) %in% dv)]
+rv
+} 
+
+Phaser<- function(fp) {
+  psav<- grep("\\.sav",fp)
+  pcsv<- grep("\\.csv",fp)
+  pdbf<- grep("\\.dbf",fp)
+  dval<- if (length(psav)>0) {library(memisc)
+    data.frame(as.data.set(spss.system.file(fp),stringsAsFactors=F))}
+    if (length(psav)>0) return(dval)
+  cval<-   if (length(pcsv)>0) (read.csv(fp,sep=",",header=T,as.is=T))
+    if (length(pcsv)>0) return(cval)
+  dbfl<- if (length(pdbf>0)) {library(foreign)
+    read.dbf(fp,as.is=T)}
+    if (length(pdbf)>0) return(dbfl)
+}
+#Phaser enhancements
+  #EXCEL HANDLER
+  #AUTOMATIC NA-RECODING
+  #Large file detection + Handling
+    #Get file size
+    #gET FF PACKAGE knowledge
+  
+#HSPAM takes a dataframe and rips out the numeric variables
+#Then produces a bunch of histograms for each one dumps it onto HDD Data
+ hspam<- function(df) {
+   library(ggplot2)
+  nums<- sapply(df,is.numeric)
+  nt<- which(nums=="TRUE")
+  dfnt<- df[,nt]
+ HS <- function(x, na.rm = TRUE, ...) {
+  nm <- names(x)
+  for (i in seq_along(nm)) {
+plots <-ggplot(x,aes_string(x = nm[i])) + geom_histogram(fill = "tomato") +
+  theme_bw() + 
+  theme(panel.grid.major=element_blank(),panel.grid.minor=element_blank())
+ggsave(plots,filename=paste("G:/DATA/",nm[i],".png",sep=""))
+  }
+ }
+ HS(dfnt)
+}
+
+#For nice-looking numeric citations in text, twopct takes a count and a total to create a string value representing the percent
+twopct<- function(subset,total) {
+  paste(round(((subset/total)*100),2),"%",sep="")
+}
+#Clean percent takes same arguments and returns number only, suitable for tables
+clnpct<- function(subset,total) {
+  round(((subset/total)*100),2)
+}
+
+#DESCENDING SORT of a data frame based on a single column value var.
+  #NOTE The var has to be references explicitly as $var
+DSORT<- function(df,var) {df[with(df, order(-var)), ]}
+ASORT<- function(df,var) {df[with(df, order(var)), ]}
+
+#Spiderman takes a dataframe and a vector of column identifiers
+#And converts directly to a sparse matrix.
+Spiderman<- function(data,dcols) {
+library(Matrix)
+nre<- nrow(data)
+nce<- length(dcols)
+mat<- matrix(0,nrow=nre,ncol=nce)
+for (i in dcols) {
+  mat[,dcols[i]]<- data[,dcols[i]]
+}
+smat<- Matrix(mat,sparse=T)
+smat
+}
+
+#' Convert a list of vectors to a data frame.
+#' 
+#' This function will convert a list of vectors to a data frame. This function
+#' will handle three different types of lists of vectors. First, if all the elements
+#' in the list are named vectors, the resulting data frame will have have a number
+#' of columns equal to the number of unique names across all vectors. In cases
+#' where some vectors do not have names in other vectors, those values will be
+#' filled with \code{NA}.
+#' 
+#' The second case is when all the vectors are of the same length. In this case,
+#' the resulting data frame is equivalent to applying \code{rbind} across all elements.
+#' 
+#' The third case handled is when there are varying vector lengths and not all the
+#' vectors are named. This condition should be avoided. However, the function will
+#' attempt to convert this list to a data frame. The resulting data frame will have
+#' a number of columns equal to the length of the longest vector. For vectors with
+#' length less than this will fill the row with \code{NA}s. Note that this function
+#' will print a warning if this condition occurs.
+#' 
+#' @author Jason Bryer <<jason@@bryer.org>>
+#' @references \url{http://stackoverflow.com/questions/4227223/r-list-to-data-frame}
+#' @param x a list to convert to a data frame.
+#' @param row.names a vector equal to \code{length(x)} corresponding to the row names.
+#'        If \code{NULL}, the row names will be set to \code{names(x)}.
+#' @param optional not used.
+#' @param ... other parameters passed to \code{\link{data.frame}}.
+#' @return a data frame.
+#' @S3method as.data.frame list
+#' @export
+#' @examples
+#'     test1 <- list( c(a='a',b='b',c='c'), c(a='d',b='e',c='f'))
+#'     as.data.frame(test1)
+#'     
+#'     test2 <- list( c('a','b','c'), c(a='d',b='e',c='f'))
+#'     as.data.frame(test2)
+#'     
+#'     test3 <- list('Row1'=c(a='a',b='b',c='c'), 'Row2'=c(var1='d',var2='e',var3='f'))
+#'     as.data.frame(test3)
+#'     
+#'     test4 <- list('Row1'=letters[1:5], 'Row2'=letters[1:7], 'Row3'=letters[8:14])
+#'     as.data.frame(test4)
+#'     
+#'     test5 <- list(letters[1:10], letters[11:20])
+#'     as.data.frame(test5)
+#'     
+#'     test6 <- list(list(letters), letters)
+#'     as.data.frame(test6)
+as.data.frame.list <- function(x, row.names=NULL, optional=FALSE, ...) {
+	if(!all(unlist(lapply(x, class)) %in% 
+				c('raw','character','complex','numeric','integer','logical'))) {
+		warning('All elements of the list must be a vector.')
+		NextMethod(x, row.names=row.names, optional=optional, ...)
+	}
+	allequal <- all(unlist(lapply(x, length)) == length(x[[1]]))
+	havenames <- all(unlist(lapply(x, FUN=function(x) !is.null(names(x)))))
+	if(havenames) { #All the vectors in the list have names we can use
+		colnames <- unique(unlist(lapply(x, names)))
+		df <- data.frame(matrix(
+			unlist(lapply(x, FUN=function(x) { x[colnames] })),
+			nrow=length(x), byrow=TRUE))
+		names(df) <- colnames
+	} else if(allequal) { #No names, but are of the same length
+		df <- data.frame(matrix(unlist(x), nrow=length(x), byrow=TRUE), ...)
+		hasnames <- which(unlist(lapply(x, FUN=function(x) !is.null(names(x)))))
+		if(length(hasnames) > 0) { #We'll use the first element that has names
+			names(df) <- names(x[[ hasnames[1] ]])
+		}
+	} else { #No names and different lengths, we'll make our best guess here!
+		warning(paste("The length of vectors are not the same and do not ",
+					"are not named, the results may not be correct.", sep=''))
+		#Find the largest
+		lsizes <- unlist(lapply(x, length))
+		start <- which(lsizes == max(lsizes))[1]
+		df <- x[[start]]
+		for(i in (1:length(x))[-start]) {
+			y <- x[[i]]
+			if(length(y) < length(x[[start]])) {
+				y <- c(y, rep(NA, length(x[[start]]) - length(y)))
+			}
+			if(i < start) {
+				df <- rbind(y, df)
+			} else {
+				df <- rbind(df, y)
+			}
+		}
+		df <- as.data.frame(df, row.names=1:length(x))
+		names(df) <- paste('Col', 1:ncol(df), sep='')
+	}
+	if(missing(row.names)) {
+		row.names(df) <- names(x)
+	} else {
+		row.names(df) <- row.names
+	}
+	return(df)
+}
+
+if(FALSE) { #Test the function
+	require(devtools)
+	source_gist(4676064)
+	
+	test1 <- list( c(a='a',b='b',c='c'), c(a='d',b='e',c='f'))
+	as.data.frame(test1)
+	
+	test2 <- list( c('a','b','c'), c(a='d',b='e',c='f'))
+	as.data.frame(test2)
+
+	test3 <- list('Row1'=c(a='a',b='b',c='c'), 'Row2'=c(a='d',var2='e',var3='f'))
+	as.data.frame(test3)
+
+	test4 <- list('Row1'=letters[1:5], 'Row2'=letters[1:7], 'Row3'=letters[8:14])
+	as.data.frame(test4)
+	
+	test5 <- list(letters[1:10], letters[11:20])
+	as.data.frame(test5)
+	
+	test6 <- list(list(letters), letters)
+	as.data.frame(test6)
+}
+
+
+#PLACE API Handler
+#GPAPI Takes a string and an API key and constructs a google place API Query.  
+#NEXT STEP:  Let gpapi execute it and store the resulting object.  
+#############DEVELOPMENT:#####################
+gpapi<- function(query,key) {
+  library(stringr)
+  library(jsonlite)
+  #Split the query object by whitespace
+  sq<- str_split(query,pattern=" ")
+  csq<- as.character(sq[[1]])
+  lcsq<- length(csq)
+  xcsq<- csq[-lcsq]
+  newe<- paste(xcsq,"+",sep="")
+  newg<- c(newe,csq[lcsq])
+  fulle<- paste(newg,collapse="")
+  stich<- paste("https://maps.googleapis.com/maps/api/place/textsearch/json?query=",
+  fulle,"&key=",key,sep="")
+  FULLAPI<- fromJSON(stich)
+  FULLAPI
+}
