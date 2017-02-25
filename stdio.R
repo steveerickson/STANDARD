b3d7d0ecf239e7cd53ffd074b308f9290d3fd062
Devin Gilmore <devingilmorecp@gmail.com>
2017-02-25 16:26:18 -0500

    Added the MFtract function:  Takes a data frame the census would give ya, and creates a block group-level GEOID based on state county tract and bg

--- stdio.R
+++ stdio.R
@@ -1,4 +1,49 @@
 
+
+#take the whole data frame and convert a new GEOID based on state,county,tract,and blockgroup
+MFtract <- function(df) {
+  a <- "0"
+  b <- "00"
+  c <- "000"
+  d <- "0000"
+  e <- "00000"
+  tracts <- df$tract
+  v <- c(a,b,c,d,e)
+  
+  z <- function(tract,v) {
+    x <- nchar(tract)
+    n5 <- paste0(v[5],tract)
+    n4 <- paste0(v[4],tract)
+    n3 <- paste0(v[3],tract)
+    n2 <- paste0(v[2],tract)
+    n1 <- paste0(v[1],tract)
+    
+    if (x==1) {return(n5)}
+    if (x==2) {return(n4)}
+    if (x==3) {return(n3)}
+    if (x==4) {return(n2)}
+    if (x==5) {return(n1)}
+    if (x==6) {return(tract)}
+  }
+  
+  
+  pull <- function(tracts, v) {
+    k <- list()
+    for (i in 1:length(tracts)) {
+      zotracts <- z(tracts[i], v)
+      k[[i]] <- zotracts
+    }
+    return(k)
+  }
+  
+  rval <- pull(tracts=tracts,v=v)
+  ln <- lapply(rval, as.character)
+  df$tractnew <- unlist(ln)
+  df$GEO.ID2  <- paste0(df$state,"0",df$county,df$tractnew,df$blockgroup)
+  return(df)
+}
+
+
 #WRITE! WRITE!   Quick write function, takes objc and fn, and if fn is not specified 
 #It executes a quickwrite 
 ww <- function(obj, fn=NULL) {
@@ -10,14 +55,44 @@ ww <- function(obj, fn=NULL) {
 
 
 
+##################000000000000000000000000 MISSING DATA
+#
+##################000000000000000000000000 MISSING DATA
 
+#STANDARD NA REPLACER 
+NAS <- function(df, replace) {
+  df[is.na(df)] <- replace
+  
+  return(df)
+}
 #Qna takes a df and returns the percent missing for each variable.  Handy!
 qna <- function(df) {
   sumna <- sapply(df, function(x) sum(as.numeric(is.na(x))))
   pctnas <- (sumna/nrow(df))*100
-  return(pctnas)
+  shlist <- list(VariableName=names(pctnas), Percents=pctnas)
+  class(shlist) <- "qna"
+  return(shlist)
 }
 
+print.qna <- function(qna) {
+  vn <- qna$VariableName
+  vpcts <- qna$Percents
+g <-   for (i in 1:length(vn)) {
+    print("-----------------------------")
+    print(paste0("VARIABLE:         ", vn[i]))
+    cat("\n")
+    print(paste0("PERCENT MISSING:  ", round(vpcts[i],2), "%"))
+    print("-----------------------------")
+    cat("\n")
+  }
+ return(g)
+} 
+#000000000000000000000000 MISSING DATA
+print("MISSING DATA FUNCTION LOADED:  qna")
+print("Qna takes a df and returns the percent missing for each variable.  Handy!")
+print("MISSING DATA FUNCTION LOADED:  NAS")
+print("NAS takes a df and a replacement value")
+##################000000000000000000000000 MISSING DATA
 
 #Full file list.
 #ENHANCE:  it'd be nice if theprint was prettier, and it listed the "FILE TYPE" 
